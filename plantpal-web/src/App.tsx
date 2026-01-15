f07de8c0312b502c4ed961a1676b3741a5d9b718
yonash <yonash@kth.se>
2026-01-15 23:07:24 +0000

    Login skeleton

--- plantpal-web/src/App.tsx
+++ plantpal-web/src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Login from "./pages/Login";
 
 function App() {
-  return "hello world";
+  return <Login />;
 }
 
 export default App;

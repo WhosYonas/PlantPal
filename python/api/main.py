01b77db0d8c08cf93d7405571bbeca281239e171
yonash <yonash@kth.se>
2026-01-15 23:34:20 +0000

    for python

--- python/api/main.py
+++ python/api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from routers import users, plants, watering_events
 from database import init_db
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Include all routers
 app.include_router(users.router, prefix="/users", tags=["Users"])

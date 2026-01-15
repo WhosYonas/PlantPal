b9d9f39e3b686819e3aed1fccb4d04ebe099c7fb
yonash <yonash@kth.se>
2026-01-15 22:46:38 +0000

    app

---
+++ plantpal-web/src/API/client.tsx
@@ -0,0 +1,14 @@
+const API_URL = "http://localhost:8000";
+
+export async function apiFetch(path: string, options: RequestInit = {}) {
+  const token = localStorage.getItem("token");
+
+  return fetch(`${API_URL}${path}`, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      ...options.headers,
+    },
+  });
+}
